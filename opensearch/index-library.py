167e5500a8ebe383f664782a84e75a155ee2647a
ft <seniorlaguna@lagunabrothers.de>
2023-07-25 12:07:52 +0900

    python indexes everything

---
+++ opensearch/index-library.py
@@ -0,0 +1,148 @@
+import os
+import logging
+from os.path import exists, join, isdir, isfile
+import json
+import requests
+
+class Material:
+    loggingEnabled: bool = False
+
+    @staticmethod
+    def enableLogging(flag: bool):
+        Material.loggingEnabled = flag
+
+    def __init__(self, id: str, enableLogging = True):
+        self.id = id
+        self.info = None
+
+        if self.__checkPrerequisites():
+            self.__populateInfoJson()
+            self.__addThumbnails()
+
+    def __checkPrerequisites(self) -> bool:
+        self.__log("Checking material " + self.id, logging.DEBUG)        
+        if not self.__checkInfoJson():
+            return False
+        if not exists(f"{self.id}/thumbnails") or not isdir(f"{self.id}/thumbnails"):
+            self.__log("doesn't contain a thumbnails directory")
+            return False
+
+        return True
+
+    def __checkInfoJson(self) -> bool:
+        path = join(self.id, "info.json")
+        if not exists(path) or not isfile(path):
+            self.__log(f"info.json doesnt exist for material {self.id}", level=logging.ERROR)
+            return False
+        
+        info = None
+        with open(path, "r") as file:
+            text = file.read()
+            info = json.loads(text)
+        
+        if info is None:
+            self.__log(f"Couldnt open info.json", logging.ERROR)
+            return False
+
+        if not self.__checkInfoJsonType(info):
+            return False
+        
+        if not self.__checkInfoJsonFields(info):
+            return False
+        
+        self.info = info
+        return True
+
+    def __checkInfoJsonType(self, info) -> bool:
+        """
+        Checks if the info.json file is a dictonary
+        """
+        if type(info) is not dict:
+            self.__log(f"material {self.id} has a invalid info.json: {info}", logging.ERROR)
+            return False
+        
+        return True
+
+    def __checkInfoJsonFields(self, info: dict) -> bool:
+        """
+        Checks if all required fields are in the info.json
+        """
+
+        # Requirements (key, type, optional check, error message for check)
+        requirements = [
+            ("name", str, lambda x: len(x) > 0, "Material name must not be empty"),
+            ("description", str, lambda x: len(x) > 0, "Description must not be empty"),
+            ("subjects", list, lambda x: all(type(i) == str and len(i) > 0 for i in x), "All subjects must be of type string and must not be empty"),
+            ("grades", list, lambda x: all(type(i) == int for i in x), "All grades must be of type int"),
+            ("tags", list, lambda x: all(type(i) == str and len(i) > 0 for i in x), "All tags must be of type string and must not be empty"),
+            ("type", str, lambda x: len(x) > 0, "Type must not be empty"),
+            ("persons", list, lambda x: True, ""),
+            ("file", str, lambda x: len(x) > 0, "File must not be empty")
+        ]
+
+        for (k, t, c, m) in requirements:
+            try:
+                value = info[k]
+                if (type(value) is not t):
+                    raise ValueError(f"Material type must be {t}")
+                if (not c(value)):
+                    raise ValueError(m)
+            except KeyError as e:
+                self.__log(f"info.json must contain the field: {e}", logging.ERROR)
+                return False
+            except ValueError as e:
+                self.__log(str(e), logging.ERROR)
+                return False
+        
+        return True
+
+    def __populateInfoJson(self):
+        self.info["name_completion"] = self.info["name"]
+        self.info["text_content"] = ""
+
+    def __addThumbnails(self):
+        self.info["thumbnails"] = []
+        for file in os.scandir(f"{self.id}/thumbnails"):
+            if file.is_file():
+                self.info["thumbnails"].append(file.name)
+
+    def __log(self, msg, level=logging.INFO):
+        if Material.loggingEnabled:
+            logging.log(level, f"[Material {self.id}] {msg}")
+
+    def __str__(self):
+        if self.info is None:
+            return ""
+
+        return f'{{ "index": {{ "_index": "materials", "_id": {self.id} }} }}\n' + \
+               json.dumps(self.info) + "\n"
+
+def main():
+    materials: list[Material] = []
+    for entry in os.scandir("./"):
+        if entry.is_dir() and entry.name.isnumeric():
+            materials.append(Material(entry.name))
+
+    data = ""
+    correctMaterials = 0
+ 
+    for material in materials:
+        if material.info is not None:
+            correctMaterials += 1
+            data += str(material)
+
+    headers = {"Content-Type": "application/x-ndjson"}
+    resp = requests.put("https://localhost:9200/materials/_bulk", headers=headers, auth=("admin", "admin"), verify=False, data=data)
+
+    
+    logging.info("[*] REPORT")
+    logging.info(f"[*] {correctMaterials} / {len(materials)} materials are valid")
+    if resp.status_code == 200:
+        logging.info("[*] updated index successfully")
+    else:
+        logging.error(f"[!] update failed: {resp.text}")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    Material.enableLogging(True)
+    main()
--- opensearch/index-library.sh
+++ opensearch/index-library.sh
@@ -10,6 +10,25 @@ TIKA="java -jar /Users/ft/Downloads/tika-app-2.8.0.jar -A"
 BULK="bulk.json"
 rm $BULK
 
+# jsonSet (variable key value)
+function jsonSet {
+    echo $1 | jq ".$2 = $3"
+}
+
+# jsonSetIfNotExists (variable key value)
+function jsonSetIfNotExists {
+    if [ $(echo $1 | jq ".$2") = null ]; then
+        jsonSet $1 $2 $3
+        return 0
+    fi
+    echo $1
+}
+
+# jsonAddToArray (variable key item)
+function jsonAddToArray {
+    jsonSetIfNotExists $1 $2 "[]" | jq ".$2 += [$3]"
+}
+
 # indexMaterial( ID )
 function indexMaterial {
     if [ -f $1/info.json ]; then
@@ -32,26 +51,18 @@ function parseTextContent {
 function processMaterial {
     echo "{ \"index\": { \"_index\": \"materials\", \"_id\": $1 } }" >> $BULK
 
-    # Get thumbnails
-    THUMBNAILS=""
+    INFO=$(cat $1/info.json)
+
+    # add thumbnails
     for img in $(ls $1/thumbnails/); do
-        if [ -z "$THUMBNAILS" ]; then
-            # First Thumbnail
-            THUMBNAILS="\"$img\""
-        else
-            # Else
-            THUMBNAILS="$THUMBNAILS, \"$img\""
-        fi
+        $INFO=$(jsonAddToArray "$INFO" thumbnails "\"$img\"")
     done
 
     # Get text content
+    $INFO=$(jsonSet "$INFO" name_completion "$(jq ".name" $1/info.json)\")
+    $INFO=$(jsonSet "$INFO" text_content "\"\"")
 
-    cat $1/info.json | \
-    jq ".thumbnails = [$THUMBNAILS]" | \
-    jq ".name_completion = $(jq ".name" $1/info.json)" | \
-    jq ".text_content = \"\"" | \
-    jq -c \
-    >> $BULK
+    echo $INFO | jq -c >> $BULK
 }
 
 echo "[i] Start indexing Education Matters Library"
@@ -62,6 +73,6 @@ for dir in $MATERIAL_DIRS; do
     indexMaterial $dir
 done
 
-curl -H "Content-Type: application/x-ndjson" -X PUT $OPENSEARCH/materials/_bulk -ku admin:admin --data-binary "@$BULK"
+#curl -H "Content-Type: application/x-ndjson" -X PUT $OPENSEARCH/materials/_bulk -ku admin:admin --data-binary "@$BULK"
 
 #rm $BULK
